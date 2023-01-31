80255807b33c1cc116f58b603fed78595752e338
xumengwei <xumengwei@qq.com>
2023-01-31 17:51:33 +0800

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true,
+    "vue/setup-compiler-macros": true
+  },
+  "extends": [
+    "plugin:vue/vue3-recommended",
+    "standard-with-typescript"
+  ],
+  "overrides": [],
+  "parserOptions": {
+    "parser": "@typescript-eslint/parser",
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+    "project": [
+      "./tsconfig.json"
+    ],
+    "tsconfigRootDir": "./",
+    "extraFileExtensions": [
+      ".vue"
+    ]
+  },
+  "plugins": [
+    "vue"
+  ],
+  "rules": {
+    "vue/max-attributes-per-line": 0,
+    "vue/html-self-closing": 0,
+    "space-before-function-paren": 0,
+    "semi": 0,
+    "vue/singleline-html-element-content-newline": 0,
+    "vue/multi-word-component-names": 0,
+    "no-console": "off",
+    "no-debugger": "off"
+  }
+}
---
+++ src/views/home/home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h4>宜昌城市大脑<br>实时数据概览</h4>
+    <div class="drop">下滑查看</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="stylus">
+.home
+  width 100%
+  min-height 100vh
+  background url(./head@2x.png) center top no-repeat
+  background-size 100% auto
+  &>h4
+    padding-top 67px
+    padding-left 16px
+    line-height 40px
+    font-size 32px
+    color #fff
+    font-family Kingsoft_Cloud_Font
+  .drop
+    margin-top 24px
+    margin-left 16px
+    width 103px
+    height 32px
+    line-height 32px
+    border-radius 16px
+    border 1px solid #fff
+    font-size 14px
+    color #fff
+    text-align center
+</style>
